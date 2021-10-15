ad877520e3637f6e74d815785642bf453d3d554c
KenyC <keny.chatain@gmail.com>
2021-10-15 16:18:02 +0200

    Basic Command

---
+++ MicroAlign.py
@@ -0,0 +1,19 @@
+import sublime_plugin
+
+
+class MicroAlignMultipleCommand(sublime_plugin.TextCommand):
+
+    def run(self, edit):
+        view = self.view
+        sel = view.sel()
+
+        if len(sel) == 1:
+            return
+
+        cols    = [view.rowcol(region.a)[1] for region in sel]
+        max_col = max(cols)
+
+        for (region, ncols) in zip(sel, cols):
+            length = max_col - ncols
+            view.insert(edit, region.a, ' ' * length)
+
---
+++ Readme.md
@@ -0,0 +1,13 @@
+MicroAlign
+============================
+
+A minimal alignment package for Sublime Text 3/4.
+
+## Usage
+
+ 1. Select multiple things
+ 2. Press Ctrl-Alt-A.
+ 3. => Inserts as many spaces before selection as needed so that the left edges of all selection regions are on the same columnn. 
+
+
+
