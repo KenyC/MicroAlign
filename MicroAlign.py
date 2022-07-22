461b3e9327089bdb9595ccc79393d992bf3ffed2
KenyC <keny.chatain@gmail.com>
2022-07-22 11:04:30 +0200

    Majority rule for heterogeneous selection regions

--- MicroAlign.py
+++ MicroAlign.py
@@ -10,10 +10,34 @@ class MicroAlignMultipleCommand(sublime_plugin.TextCommand):
         if len(sel) == 1:
             return
 
-        cols    = [view.rowcol(region.a)[1] for region in sel]
+        # Sometimes, multiple selections are not oriented the same way:
+        #   .----|
+        #   |---------.
+        #     |---------.
+        # This frequently happens when using Ctrl+D, and the first selection is backwards
+        # In case selection regions don't all cursor in the same position, we use a majority rule to decide whether to align with left or right boundary of the region
+        # With a preference for left boundary in case of ties (which is what one wants 67.8 % of the times)
+
+        # True if in most regions, a is before b false otherwise
+        majority_order = sum(
+            1 if region.a <= region.b else -1
+            for region in sel
+        ) >= 0
+
+        # Which position to align
+        anchors = [
+            region.a if majority_order == (region.a <= region.b)
+            else region.b
+            for region in sel
+        ]
+
+        cols = [
+            view.rowcol(anchor)[1] 
+            for anchor in anchors
+        ]
         max_col = max(cols)
 
-        for (region, ncols) in zip(sel, cols):
+        for (anchor, ncols) in zip(anchors, cols):
             length = max_col - ncols
-            view.insert(edit, region.a, ' ' * length)
+            view.insert(edit, anchor, ' ' * length)
 
