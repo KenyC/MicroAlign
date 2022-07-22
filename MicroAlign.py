93cc5bac42365e2b62858af1317f8df044e65f86
KenyC <keny.chatain@gmail.com>
2022-07-22 11:24:05 +0200

    Revert majority rule + align with left boundary

--- MicroAlign.py
+++ MicroAlign.py
@@ -10,34 +10,10 @@ class MicroAlignMultipleCommand(sublime_plugin.TextCommand):
         if len(sel) == 1:
             return
 
-        # Sometimes, multiple selections are not oriented the same way:
-        #   .----|
-        #   |---------.
-        #     |---------.
-        # This frequently happens when using Ctrl+D, and the first selection is backwards
-        # In case selection regions don't all cursor in the same position, we use a majority rule to decide whether to align with left or right boundary of the region
-        # With a preference for left boundary in case of ties (which is what one wants 67.8 % of the times)
-
-        # True if in most regions, a is before b false otherwise
-        majority_order = sum(
-            1 if region.a <= region.b else -1
-            for region in sel
-        ) >= 0
-
-        # Which position to align
-        anchors = [
-            region.a if majority_order == (region.a <= region.b)
-            else region.b
-            for region in sel
-        ]
-
-        cols = [
-            view.rowcol(anchor)[1] 
-            for anchor in anchors
-        ]
+        cols    = [view.rowcol(region.begin())[1] for region in sel]
         max_col = max(cols)
 
-        for (anchor, ncols) in zip(anchors, cols):
+        for (region, ncols) in zip(sel, cols):
             length = max_col - ncols
-            view.insert(edit, anchor, ' ' * length)
+            view.insert(edit, region.begin(), ' ' * length)
 
--- Readme.md
+++ Readme.md
@@ -7,7 +7,7 @@ A minimal alignment package for Sublime Text 3/4. Strictly worse than the compet
 
  1. Select multiple things
  2. Press <kbd>Ctrl</kbd> + <kbd>Alt</kbd> + <kbd>A</kbd> (or <kbd>&#8984;</kbd> + <kbd>Alt</kbd> + <kbd>A</kbd>).
- 3. => Inserts as many spaces before selection as needed so that the left edges of all selection regions are on the same columnn. 
+ 3. Inserts as many spaces before selection as needed so that the left edges of all selection regions are on the same columnn. 
 
-
+![demo.gif]
 
